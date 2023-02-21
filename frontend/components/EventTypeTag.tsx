f4e389460a604bce7d11f728720629a32e696248
magicalsoup <amagicalsoup@gmail.com>
2023-02-21 18:33:56 -0500

    fix?

--- frontend/components/EventTypeTag.tsx
+++ frontend/components/EventTypeTag.tsx
@@ -5,7 +5,7 @@ export default function EventTypeTag({eventTypeName}: {
     const eventTypeColor = tagColors[eventTypes.indexOf(eventTypeName)]
     
     // Doesnt work sadge
-    return <div className={`${eventTypeColor} p-2 rounded-md text-gray-100`} style={{color: eventTypeColor}}>
+    return <div className={`${eventTypeColor} p-2 rounded-md text-gray-100`}>
         {eventTypeName.replace("_", " ")} 
     </div>
 }
