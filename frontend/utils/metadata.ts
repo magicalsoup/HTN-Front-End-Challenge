13468414625aa1c3096c55fd97ece358935c0183
magicalsoup <amagicalsoup@gmail.com>
2023-02-22 13:16:34 -0500

    fix colors v2

--- frontend/utils/metadata.ts
+++ frontend/utils/metadata.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINT = "https://api.hackthenorth.com/v3/";
 
-const tagColors = ["bg-red-400", "rgb(251 146 60);", "bg-purple-400"]; // in tailwind styles for consistency
+const tagColors = ["bg-red-400", "bg-orange-400", "bg-purple-400"]; // in tailwind styles for consistency
 const eventTypes= ["workshop", "activity", "tech_talk"];
 
 export function getTagColors() {
